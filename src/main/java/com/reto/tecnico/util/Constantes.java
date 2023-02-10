1c7686d8adc9531a5048e895b69a56419e256067
danielvesar <danyvesar97@gmail.com>
2023-02-10 11:02:31 -0500

    Formateada la instrucción SQL

--- src/main/java/com/reto/tecnico/util/Constantes.java
+++ src/main/java/com/reto/tecnico/util/Constantes.java
@@ -1,6 +1,9 @@
 package com.reto.tecnico.util;
 
 public class Constantes {
-	 /* Constantes SQL para la búsqueda usada en el index.html*/
-		public static final String CONSULTA_GENERAL="SELECT n FROM Nave n INNER JOIN ClaseNave cn ON n.tipo=cn.id INNER JOIN Combustible cb ON n.combustible=cb.id WHERE CONCAT(n.id,n.nombre,n.paisOrigen,n.altura,n.peso) LIKE %?1% OR CONCAT(cn.id,cn.tipoNave) LIKE %?1% OR CONCAT(cb.id,cb.tipoCombustible) LIKE %?1%";
-	}
+	/* Constantes SQL para la búsqueda usada en el index.html */
+	public static final String CONSULTA_GENERAL = 
+			"SELECT n FROM Nave n INNER JOIN ClaseNave cn ON n.tipo=cn.id INNER JOIN Combustible cb ON n.combustible=cb.id "
+			+ "WHERE CONCAT(n.id,n.nombre,n.paisOrigen,n.altura,n.peso) LIKE %?1% "
+			+ "OR CONCAT(cn.id,cn.tipoNave) LIKE %?1% OR CONCAT(cb.id,cb.tipoCombustible) LIKE %?1%";
+}
